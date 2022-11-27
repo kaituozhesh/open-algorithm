53c4fb20074419f3ee549ab2d75f8439c68edc58
林俊豪 <linjh@olymtech.com>
2022-11-27 09:57:57 +0800

    二分查找法非递归实现

---
+++ general-algorithm/src/main/java/com/open/ityizhan/general/binaryserch/BinarySearch.java
@@ -0,0 +1,38 @@
+package com.open.ityizhan.general.binaryserch;
+
+/**
+ * 二分查找法的非递归实现
+ *
+ * @ClassName : BinarySearch
+ * @Description :
+ * @Author : 林俊豪
+ * @Date: 2022-11-27 09:44
+ * @Version: 1.0.0
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 111, 222, 333, 444, 555, 666, 777, 888, 999};
+        System.out.println(search(arr, 33));
+    }
+
+    public static int search(int[] arr, int num) {
+
+        if (arr.length == 0) {
+            throw new IllegalArgumentException("Arr is empty");
+        }
+        int l = 0;
+        int r = arr.length - 1;
+        int mid = l + (r - l) / 2;
+        while (arr[mid] != num) {
+            if (arr[mid] > num) {
+                r = mid - 1;
+                mid = l + (r - l) / 2;
+            } else {
+                l = mid + 1;
+                mid = l + (r - l) / 2;
+            }
+        }
+        return mid;
+    }
+}
